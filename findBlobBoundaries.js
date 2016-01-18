dbb1e837f1c1e6a4943d6b4fcbd1a69a008343ce
Marco Pariente-Cohen <markypc3@gmail.com>
2016-01-18 12:45:39 -0500

    added findBlobCellNeighbors method which minimally checks all neighbors of a given cell within a given array

--- findBlobBoundaries.js
+++ findBlobBoundaries.js
@@ -10,8 +10,37 @@ function findFirstBlobCell(sample) {
   }
 }
 
+function findBlobCellNeighbors(sample, blobTop, cell) {
+  var neighbors = [];
+  if( cell.i != blobTop && cell.i > 0){
+    if (sample[cell.i - 1][cell.j] === 1){
+      neighbors.push({i: cell.i - 1, j: cell.j});
+    }
+  }
+  if ( cell.j > 0 ) {
+    if (sample[cell.i][cell.j - 1] === 1){
+      neighbors.push({i: cell.i, j: cell.j - 1});
+    }
+  }
+  if ( cell.j < sample[cell.i].length - 1){
+    if (sample[cell.i][cell.j + 1] === 1){
+      neighbors.push({i: cell.i, j: cell.j + 1});
+    }
+  }
+  if ( cell.i < sample.length - 1 ){
+    if (sample[cell.i + 1][cell.j] === 1) {
+      neighbors.push({i: cell.i + 1, j: cell.j});
+    }
+  }
+  return neighbors;
+}
+
 function findBlobBoundaries(sample) {
-  console.log(findFirstBlobCell(sample));
+  var firstCell = findFirstBlobCell(sample);
+  var top = firstCell.j;
+  var blobCells = [firstCell];
+  console.log(findBlobCellNeighbors(sample, top, blobCells[0]));
+
 }
 
 findBlobBoundaries(sample);
