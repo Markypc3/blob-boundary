bf0440a68656fd2cf2a9595affafc718a313d233
Marco Pariente-Cohen <markypc3@gmail.com>
2016-01-18 12:13:50 -0500

    implemented the findFirstBlobCell function that now returns the i and j coordinates of the uppermost leftmost cell of the blob

--- findBlobBoundaries.js
+++ findBlobBoundaries.js
@@ -1,2 +1,17 @@
 var sample = require('./sample.js');
-console.log(sample);
+
+function findFirstBlobCell(sample) {
+  for (var i = 0; i < sample.length; i++) {
+    for(var j = 0; j <= sample[i].length; j++) {
+      if (sample[i][j] === 1) {
+        return { i: i, j: j}
+      }
+    }
+  }
+}
+
+function findBlobBoundaries(sample) {
+  console.log(findFirstBlobCell(sample));
+}
+
+findBlobBoundaries(sample);
